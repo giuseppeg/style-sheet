e1b57ed5917c2d2ea16b8c798b1ff7199be71b00
Giuseppe Gurgone <giuseppeg@users.noreply.github.com>
2018-10-06 19:00:01 +0200

    better e2e setup

--- test/e2e/_setup.js
+++ test/e2e/_setup.js
@@ -3,10 +3,18 @@ const test = require('ava')
 const puppeteer = require('puppeteer')
 
 global.test = test
+global.debug = false
 
 test.before(async () => {
-  global.browser = await puppeteer.launch()
+  global.browser = await puppeteer.launch({ headless: !global.debug })
   global.page = await browser.newPage()
+  global.gotoPage = async fileName => {
+    await page.goto('http://localhost:5000/' + fileName, { waitUntil: 'load' })
+    await page.addScriptTag({ url: 'http://localhost:5000/_helpers.js' })
+    await page.addScriptTag({
+      url: 'http://localhost:5000/dist/_styleSheet.js',
+    })
+  }
 })
 
 test.after.always(async () => {
--- test/e2e/test.js
+++ test/e2e/test.js
@@ -1,19 +1,40 @@
 require('./_setup')
 
-test('page title', async t => {
-  await page.goto('http://localhost:5000/test.html', { waitUntil: 'load' })
+test('minimal testcase', async t => {
+  await gotoPage('test.html')
 
   const color = await page.evaluate(() => {
-    const root = document.querySelector('#root')
     const { StyleSheet, StyleResolver } = styleSheet.create()
     const styles = StyleSheet.create({
       root: {
         color: 'green',
       },
     })
+    const root = document.querySelector('#root')
     root.classList.add(StyleResolver.resolve(styles.root))
     return getComputedStyle(root).getPropertyValue('color')
   })
 
   t.is(color, 'rgb(0, 128, 0)')
+})
+
+// TODO Figure out why `sheets.sheet.ownerNode` is `null` after setting its `textContent` here.
+test.skip('reconciles style tags', async t => {
+  await gotoPage('test.html')
+
+  const styles = await page.evaluate(() => {
+    const sheets = styleSheet.createSheets()
+    sheets.sheet.ownerNode.textContent = '.dss_h28rbs-i0tgik {color:red}'
+
+    const { StyleSheet, StyleResolver } = styleSheet.create(sheets)
+    const styles = StyleSheet.create({
+      root: {
+        color: 'green',
+      },
+    })
+    StyleResolver.resolve(styles.root)
+    return cssRulesToString(sheets.sheet.cssRules)
+  })
+
+  t.snapshot(styles)
 })
