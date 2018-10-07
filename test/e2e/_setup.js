cd50efb2bab9233548950b3d6065f2ff2774b45b
Giuseppe Gurgone <giuseppeg@users.noreply.github.com>
2018-10-07 10:04:11 +0200

    Fix concurrent e2e tests

--- test/e2e/_setup.js
+++ test/e2e/_setup.js
@@ -5,19 +5,20 @@ const puppeteer = require('puppeteer')
 global.test = test
 global.debug = false
 
-test.before(async () => {
-  global.browser = await puppeteer.launch({ headless: !global.debug })
-  global.page = await browser.newPage()
-  global.gotoPage = async fileName => {
+global.testBefore = async () => {
+  const browser = await puppeteer.launch({ headless: !global.debug })
+  const page = await browser.newPage()
+  const gotoPage = async fileName => {
     await page.goto('http://localhost:5000/' + fileName, { waitUntil: 'load' })
     await page.addScriptTag({ url: 'http://localhost:5000/_helpers.js' })
     await page.addScriptTag({
       url: 'http://localhost:5000/dist/_styleSheet.js',
     })
   }
-})
+  return { browser, page, gotoPage }
+}
 
-test.after.always(async () => {
-  await page.close()
-  await browser.close()
-})
+global.testAfter = async context => {
+  await context.page.close()
+  await context.browser.close()
+}
--- test/e2e/test.js
+++ test/e2e/test.js
@@ -1,6 +1,9 @@
 require('./_setup')
 
 test('minimal testcase', async t => {
+  const context = await testBefore()
+  const { gotoPage, page } = context
+
   await gotoPage('test.html')
 
   const color = await page.evaluate(() => {
@@ -16,9 +19,13 @@ test('minimal testcase', async t => {
   })
 
   t.is(color, 'rgb(0, 128, 0)')
+
+  await testAfter(context)
 })
 
 test('resolves deterministically', async t => {
+  const context = await testBefore()
+  const { gotoPage, page } = context
   await gotoPage('test.html')
 
   const results = await page.evaluate(() => {
@@ -47,9 +54,13 @@ test('resolves deterministically', async t => {
   })
 
   t.is(results, '{"green":"rgb(0, 128, 0)","red":"rgb(255, 0, 0)"}')
+
+  await testAfter(context)
 })
 
 test('reconciles style tags when resolving new rules', async t => {
+  const context = await testBefore()
+  const { gotoPage, page } = context
   await gotoPage('test.html')
 
   const css = await page.evaluate(() => {
@@ -73,9 +84,13 @@ test('reconciles style tags when resolving new rules', async t => {
     css,
     '.dss_h28rbs-b5mm4 { color: green; }.dss_h28rbs-i0tgik { color: red; }'
   )
+
+  await testAfter(context)
 })
 
 test('when reconciling does not add new rules if they exist', async t => {
+  const context = await testBefore()
+  const { gotoPage, page } = context
   await gotoPage('test.html')
 
   const css = await page.evaluate(() => {
@@ -96,9 +111,13 @@ test('when reconciling does not add new rules if they exist', async t => {
   })
 
   t.is(css, '.dss_h28rbs-i0tgik { color: red; }')
+
+  await testAfter(context)
 })
 
 test('can use any style tag in the page', async t => {
+  const context = await testBefore()
+  const { gotoPage, page } = context
   await gotoPage('test.html')
 
   const results = await page.evaluate(() => {
@@ -133,9 +152,13 @@ test('can use any style tag in the page', async t => {
   })
 
   t.is(results, '{"green":"rgb(0, 128, 0)","red":"rgb(255, 0, 0)"}')
+
+  await testAfter(context)
 })
 
 test('reconciles link tags i.e. inserts only new rules', async t => {
+  const context = await testBefore()
+  const { gotoPage, page } = context
   await gotoPage('test.html')
   await page.addStyleTag({ url: '/fixtures/external.css' })
 
@@ -157,9 +180,14 @@ test('reconciles link tags i.e. inserts only new rules', async t => {
   })
 
   t.is(css, '.dss_h28rbs-b5mm4 { color: green; }')
+
+  await testAfter(context)
 })
 
 test('moves link tag to be in between the style tags', async t => {
+  const context = await testBefore()
+  const { gotoPage, page } = context
+
   await gotoPage('test.html')
   await page.addStyleTag({ url: '/fixtures/external.css' })
 
@@ -176,4 +204,6 @@ test('moves link tag to be in between the style tags', async t => {
   })
 
   t.is(html, 'STYLE|LINK|STYLE')
+
+  await testAfter(context)
 })
